abf6b0afe3bd7e1f36235c9a1b22523b24022362
AphroditiMaria <afroditi-maria.zaki@bioch.ox.ac.uk>
2023-07-18 17:17:48 +0100

    Updated analysis script

--- IonPermeation.py
+++ IonPermeation.py
@@ -73,12 +73,42 @@ def ion_positions(ag):
     # sq_z_pos = z_pos
     return z_pos
 
+def channel_limits(in_sel, out_sel):
+    """
+    Define entry and exit point of ion channel
+    """
+    z_in = in_sel.center_of_mass()[2]
+    z_out = out_sel.center_of_mass()[2]
+
+    return z_in, z_out
+
+def pore_center(in_sel, out_sel):
+    """
+    Define center of pore axis
+    """
+    v1 = in_sel.center_of_mass()
+    v2 = out_sel.center_of_mass()
+    v3 = v2 - v1
+
+    return v3
+
+def in_cylinder(atomgroup, out_sel, cutoff=5.0):
+    rcp = np.array([out_sel.center_of_mass()[0], out_sel.center_of_mass()[1]])
+    for atom in atomgroup:
+        r = np.array([atom.position[0], atom.position[1]])
+        dist = np.linalg.norm(rcp-r)
+        if dist <= cutoff:
+            print(atom.index, atom.position[2])
+
+
 class IonPermeation(AnalysisBase):
-    def __init__(self, atomgroup, verbose=True):
+    def __init__(self, atomgroup, in_sel, out_sel, verbose=True):
         super(IonPermeation, self).__init__(atomgroup.universe.trajectory, verbose=verbose)
         # self.u = universe
         # self._trajectory = self.u.trajectory
         self.atomgroup = atomgroup
+        self.in_sel = in_sel
+        self.out_sel = out_sel
         # self.results = Results()
         # self._trajectory = self.u.trajectory
         # self._parameter = cutoff
@@ -132,6 +162,9 @@ class IonPermeation(AnalysisBase):
         
         # z_pos = ion_positions(self)
         res = ion_positions(self.atomgroup)
+        # axis = pore_axis(self.in_sel, self.out_sel)
+        pore_ions = in_cylinder(self.atomgroup, self.out_sel)
+        # print(pore_ions)
         # save it into self.results
         self.results[self._frame_index, 1:] = res
         # the current timestep of the trajectory is self._ts
@@ -221,29 +254,8 @@ path = "/biggin/b134/bioc1550/Documents/WORK/GITHUB/IONPERMEATION"
 
 u = mda.Universe(f"{path}/test.pdb", f"{path}/test.xtc")
 ion = u.select_atoms("resname SOD")
-print(len(ion))
-# # print(ion_sel.positions)
-
-ip = IonPermeation(ion, verbose=True).run()
-ip.plot()
-# print(ip.ion_positions())
-# print(ip._prepare())
-# print(ip._conclude())
-# print(ip._single_frame())
+in_sel = u.select_atoms("protein and resid 652 653 654 and name CA")
+out_sel = u.select_atoms("protein and resid 690 694 and name CA")
 
 
-# print(IonPermeation.results)
-# print(IonPermeation(universe=u, atomgroup="resname SOD")._single_frame())
-# IonPermeation.
-# ip.run(start=0, stop=101, step=10)
-# print(ip.results)
-
-
-
-# # permeation = IonPermeation(universe=u,
-# #                            ion_sel="resname SOD",
-# #                            in_sel="protein and resid 652-654",
-# #                            out_sel="protein and resid 308 312 690 694 and name CA")
-
-# # permeation.ion_positions()
-# # permeation.in_cylinder()
+ip = IonPermeation(ion, in_sel, out_sel, verbose=True).run()
