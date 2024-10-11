ab86b443562736ade439dced9dc0f907d6edc875
AfroditiMariaZaki <afroditi-maria.zaki@bioch.ox.ac.uk>
2024-10-11 14:12:27 +0100

    Added test_IonPermeation.py file in tests folder

---
+++ IonPermeation/tests/test_IonPermeation.py
@@ -0,0 +1,31 @@
+# import pytest
+import MDAnalysis as mda
+
+
+
+def setup_universe():
+    """
+    Create a sample Universe with a test PDB and trajectory file
+    """
+    topfile = "../data/test.pdb"
+    trjfile = "../data/test.xtc"
+    u = mda.Universe(topfile, trjfile)
+
+#     return u
+
+# def initialization(setup_universe):
+#     """
+#     Initialize the IonPermeation module
+#     """
+#     u = setup_universe()
+#     ions = "resname SOD"
+#     in_sel = "protein and resid 271 272 652 653 654 and name CA"
+#     out_sel = "protein and resid 690 694 308 312 and name CA"
+
+#     # Initialize IonPermeation
+#     IP = IonPermeation(u, ions, in_sel, out_sel)
+
+#     assert IP.u == u
+#     assert IP.atomgroup == ions
+#     assert IP.in_sel is not None
+#     assert IP.out_sel is not None
